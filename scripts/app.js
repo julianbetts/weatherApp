22203313031da150401882ec36240faf8faafedf
Julian Betts <wjulianbetts@gmail.com>
2023-10-08 16:27:04 -0700

    finnishing touches

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+const cityForm = document.querySelector('form')
+const cityName = document.getElementById('cityName')
+const cityTemp = document.getElementById('cityTemp')
+const iconEl = document.getElementById('image')
+
+
+const updateCity = async (city) => {
+
+    const cityDets = await getCity(city)
+    const weather = await getWeather(cityDets.Key)
+
+    return {
+        cityDets: cityDets,
+        weather: weather
+    }
+}
+
+const showWeatherData = (temp, name, iconID) => {
+    //add city name to HTML
+    cityTemp.innerText = temp
+    //add temperature to HTML
+    cityName.innerText = name
+    showIcon(iconID)
+}
+
+const showIcon = (iconNumber) => {
+    iconEl.src = '/img/' + iconNumber + '.svg'
+}
+
+
+cityForm.addEventListener('submit', e => {
+    //prevent default action
+    e.preventDefault()
+
+    // get city value
+    const cityObject = cityForm.city.value.trim()
+    cityForm.reset()
+
+    //update the ui with the new city
+    updateCity(cityObject)
+        .then(data => showWeatherData(data.weather.Temperature.Imperial.Value, data.cityDets.LocalizedName, data.weather.WeatherIcon))
+        .catch(err => console.log(err))
+})
+
+
+
